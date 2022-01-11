1db0a53b42f631799040230331211801b15e73f4
Teemu Koivisto <tkoivisto456@gmail.com>
2022-01-11 12:00:03 +0200

    refactor(utils)

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -6,7 +6,7 @@ export function uuidv4() {
   //   // @ts-ignore
   //   return crypto.randomUUID()
   // }
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0,
       v = c == 'x' ? r : (r & 0x3) | 0x8
     return v.toString(16)
